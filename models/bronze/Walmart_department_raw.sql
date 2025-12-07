9bcd1ba9cd935ea77e5f20d0971dc447fc0fdb62
Hadi Hosseini <snowflake120@mail.com>
2025-12-07 06:22:47 +0000

    fisnish the brnoze layer table creation

---
+++ models/bronze/Walmart_department_raw.sql
@@ -0,0 +1,31 @@
+/* Walmart_department_raw: */
+{% set columns_table_department = {
+    'STORE': 1,
+    'DEPT': 2,
+    'DATE': 3,
+    'WEEKLY_SALES': 4,
+    'ISHOLIDAY': 5
+} %}
+
+{{ config({ "materialized":'table',
+            "transient":true,
+            "alias":'DEPARTMENT',
+            "pre_hook": macros_copy_csv('DEPARTMENT', columns_table_department, '.*department.*\\.csv'),
+            "schema": 'BRONZE' }) }}
+
+WITH transform_2 AS (
+    SELECT 
+        STORE,
+        DEPT,
+        DATE,
+        WEEKLY_SALES,
+        ISHOLIDAY,
+        INSERT_DTS AS INSERT_DTS,
+        UPDATE_DTS AS UPDATE_DTS,
+        SOURCE_FILE_NAME AS SOURCE_FILE_NAME,
+        SOURCE_FILE_ROW_NUMBER AS SOURCE_FILE_ROW_NUMBER
+    FROM {{source('department_source','DEPARTMENT')}}
+    )
+
+SELECT *
+FROM transform_2
--- models/silver/Walmart_store_dim.sql
+++ models/silver/Walmart_store_dim.sql
@@ -1,20 +1,48 @@
 /* Walmart_store_dim: */
-{% set columns_table = {
+{% set columns_table_sotres = {
     'STORE': 1,
     'TYPE': 2,
-    'SIZE': 3 } %}
+    'SIZE': 3
+} %}
+
+{% set columns_table_department = {
+    'STORE': 1,
+    'DEPT': 2,
+    'DATE': 3,
+    'WEEKLY_SALES': 4,
+    'ISHOLIDAY': 5
+} %}
+
+{% set columns_table_fact = {
+    'STORE': 1,
+    'DATE': 2,
+    'TEMPERATURE': 3,
+    'FUEL_PRICE': 4,
+    'MARKDOWN1': 5,
+    'MARKDOWN2': 6,
+    'MARKDOWN3': 7,
+    'MARKDOWN4': 8,
+    'MARKDOWN5': 9,
+    'CPI': 10,
+    'UNEMPLOYMENT': 11,
+    'ISHOLIDAY': 12
+} %}
 
 {{ config({ "materialized":'table',
             "transient":true,
             "alias":'STORES',
-            "pre_hook": [macros_copy_csv('STORES', columns_table, '.*stores.*\\.csv')],
+            "pre_hook": [
+                macros_copy_csv('STORES', columns_table_sotres, '.*stores.*\\.csv'),
+                macros_copy_csv('DEPARTMENT', columns_table_department, '.*department.*\\.csv'),
+                macros_copy_csv('FACT', columns_table_fact, '.*fact.*\\.csv')
+            ],
             "schema": 'SILVER' }) }}
 
-WITH transform AS (
+WITH transform_1 AS (
     SELECT 
-        STORE AS STORE,
-        TYPE AS TYPE,
-        SIZE AS SIZE,
+        STORE,
+        TYPE,
+        SIZE,
         'EST' AS TIME_ZONE,
         'PRODUCT' AS SOURCE_SYS_NAME,
         'STANDARD' AS INSTNC_ST_NM,
@@ -26,6 +54,6 @@ WITH transform AS (
         SOURCE_FILE_ROW_NUMBER AS SOURCE_FILE_ROW_NUMBER
     FROM {{source('source','STORES')}}
     )
- 
+
 SELECT *
 FROM transform
