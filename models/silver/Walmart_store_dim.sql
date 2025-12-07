62922ce90c12d519f71130262b34c0cc797ba7b7
Hadi Hosseini <snowflake120@mail.com>
2025-12-07 00:00:15 +0000

    create the first xfm model

---
+++ models/silver/Walmart_store_dim.sql
@@ -0,0 +1,27 @@
+### Walmart_store_dim:
+{{ config({ "materialized":'table',
+            "transient":true,
+            "alias":STORES,
+            "pre_hook": macros_copy_csv('STORES', ['STORE', 'TYPE', 'SIZE']),
+            "schema": 'SILVER' }) }}
+ 
+WITH transform AS (
+    SELECT 
+        STORE AS STORE,
+        TYPE AS TYPE,
+        SIZE AS SIZE,
+        'EST' AS TIME_ZONE,
+        'PRODUCT' AS SOURCE_SYS_NAME,
+        'STANDARD' AS INSTNC_ST_NM,
+        CURRENT_SESSION() AS PROCESS_ID,
+        'TRANSFORM_LOAD' AS PROCESS_NAME,
+        INSERT_DTS AS INSERT_DTS,
+        UPDATE_DTS AS UPDATE_DTS,
+        SOURCE_FILE_NAME AS SOURCE_FILE_NAME,
+        SOURCE_FILE_ROW_NUMBER AS SOURCE_FILE_ROW_NUMBER,
+    FROM {{source('source','STORES')}}
+    )
+ 
+SELECT *
+FROM transform
+
